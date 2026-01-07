4a3fd27f802d9a9c6964a049bb97787dbb65ba10
varshan1569-blip <varshan1569@gmail.com>
2026-01-08 00:37:58 +0530

    day 1

--- day1.js
+++ day1.js
@@ -1,3 +1,7 @@
 console.log("Day 1 JavaScript practice");
 console.log(10 + 20);	
 console.log("HEllo World!");
+
+// Let is a local variable scope declaration and var is a global scope variable declaration.
+// Once const is declared to a variable then it cant be changed
+
